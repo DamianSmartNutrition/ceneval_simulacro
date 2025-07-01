76fd0c05349de364b1f20254398d12a44232c1be
DamianSmartNutrition <damianrzmnd@gmail.com>
2025-07-01 11:43:02 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: './',  // <- Esto asegura que los paths de los assets se carguen relativos
   plugins: [react()],
-})
+});
